862944875abb94ab75beff7caa8f4bff40db3232
Jan <jjirsik@gmail.com>
2019-11-01 15:20:46 +0100

    mix fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     </head>
     <body>
